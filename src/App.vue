00d512cae1e3fd82a25d4e9a37273e333d7a1669
Fabricio Rodrigues <fabricio.rodrigues@daimler.com>
2018-01-30 17:08:20 +0100

    Some styling

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,9 @@ export default {
 </script>
 
 <style>
+html {
+  font-size: 14px;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <ul>
       <li v-for="post in posts" :key="post.id">
         <router-link :to="`/posts/${post.id}`">{{post.title}}</router-link>
+        <p>{{post.body}}</p>
       </li>
     </ul>
   </div>
@@ -31,5 +32,29 @@ export default {
 </script>
 
 <style scoped>
-
+.home {
+  margin: 0 auto;
+  max-width: 1200px;
+}
+.home ul {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  list-style: none;
+  width: 100%;
+}
+.home ul li {
+  border: 1px solid #ebebeb;
+  border-radius: 8px;
+  height: 100px;
+  margin: 20px;
+  padding: 20px;
+  text-align: left;
+  width: 510px;
+}
+.home ul li a {
+  color: #111;
+  font-size: 1.5rem;
+  text-decoration: none;
+}
 </style>
